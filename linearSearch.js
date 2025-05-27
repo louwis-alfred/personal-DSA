8cec14cc0f9795fe73ba0d4f3d1fa618c3a6a928
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2025-05-27 15:23:03 +0800

    Create linearSearch.js

---
+++ linearSearch.js
@@ -0,0 +1,10 @@
+const numbers = [3, 7, 2, 9, 4];
+
+let max = numbers[0];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > max) {
+    max = numbers[i]
+  }
+}
+console.log("Maximum number: ", max)
